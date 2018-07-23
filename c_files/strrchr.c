c8f16fd935c5429a1037f4ce5c34800564bf97e6
Yoganandp <yoganandpathak@gmail.com>
2018-07-22 20:45:53 -0400

    Create strrchr.c

---
+++ c_files/strrchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//strrchr took 0.000000
+
+int main(void)
+{
+    const char* str = "hello world, friend of mine!";
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+        strrchr(str, 'f');		
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("strrchr took %6.6f\n", secs);
+    
+}
