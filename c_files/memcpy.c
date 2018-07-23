dd6e4de41943ed31724ad731100425b7a3065bf9
Yoganandp <yoganandpathak@gmail.com>
2018-07-22 20:23:39 -0400

    Create memcpy.c

---
+++ c_files/memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//memcpy took 0.000000000
+
+int main(void)
+{
+    char source[] = "once upon a midnight dreary...", dest[4];
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+        memcpy(dest, source, sizeof dest);		
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("memcpy took %6.9f\n", secs);
+    
+}
