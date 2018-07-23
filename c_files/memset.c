8a4b6c00b5e28fd2ba86ae9615915757569f494d
Yoganandp <yoganandpathak@gmail.com>
2018-07-22 20:25:10 -0400

    Create memset.c

---
+++ c_files/memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//memset took 0.000725000
+
+int main(void)
+{
+    char str[] = "ghghghghghghghghghghgh";
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+        memset(str,'a',5);		
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("memset took %6.9f\n", secs);
+    
+}
