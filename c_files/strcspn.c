917dc0c018ac5363713a45f8ad50d092e77c5143
Yoganandp <yoganandpathak@gmail.com>
2018-07-22 20:43:29 -0400

    Create strcspn.c

---
+++ c_files/strcspn.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//strcspn took 0.000000
+
+int main(void)
+{
+    const char* str = "hello world, friend of mine!";
+    const char* sep = " ,!";
+    struct timeval stop, start;
+    gettimeofday(&start, NULL);
+    unsigned int count;
+    for( count=0; count <= 1000000; count++) {
+        strcspn(str, sep);		
+    }
+   
+    gettimeofday(&stop, NULL);
+    double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+    printf("strcspn took %6.6f\n", secs);
+    
+}
